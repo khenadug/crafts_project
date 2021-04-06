8891f8a9df1c5eecbc4712201f7acb441cde57c6
khenadug <77457378+khenadug@users.noreply.github.com>
2021-04-06 13:54:30 +0100

    Update urls.py

--- crafts_project/urls.py
+++ crafts_project/urls.py
@@ -16,6 +16,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from crafts import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
