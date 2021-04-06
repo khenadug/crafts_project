604f1b72aa0fdb211b0219ca47e86236a5ea5635
Stuart Malcolm <2470874M@student.gla.ac.uk>
2021-04-06 13:47:06 +0100

    added views and forms that allow a user to sign up for a profile

--- crafts/views.py
+++ crafts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from crafts.models import UserProfile, Post, Comment, Message
-#from crafts.forms import [Forms]
+from crafts.forms import UserForm, UserProfileForm
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -31,6 +31,35 @@ def user_login(request):
             return HttpResponse("Invalid login details supplied.")
     else:
         return render(request, 'crafts/login.html')
+
+
+def user_signup(request):
+    registered = False
+    
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+    
+    return render(request, 
+                'crafts/signup.html', 
+                context = {'user_form': user_form,
+                            'profile_form': profile_form,
+                            'registered': registered})
 
 
 def following(request, user_name):
