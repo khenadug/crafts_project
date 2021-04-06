84a15e20e957c09fd33d09d013a53da1cd2a2cd2
khenadug <77457378+khenadug@users.noreply.github.com>
2021-04-06 14:22:06 +0100

    Update settings.py

--- crafts_project/settings.py
+++ crafts_project/settings.py
@@ -123,5 +123,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATIC_URL = '/static/'
